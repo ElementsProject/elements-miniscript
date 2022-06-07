5007c24851433bfa0be22fbfe712039dad1cb5ac
sanket1729 <sanket1729@gmail.com>
2022-06-07 14:56:42 -0700

    Fix fuzzer error: Actually a bug in merge

--- src/lib.rs
+++ src/lib.rs
@@ -152,7 +152,7 @@ use elements::secp256k1_zkp::Secp256k1;
 use elements::{opcodes, script, secp256k1_zkp};
 
 pub use crate::descriptor::{Descriptor, DescriptorPublicKey};
-use crate::extensions::{CovenantExt, Extension, NoExt};
+pub use crate::extensions::{CovenantExt, Extension, NoExt};
 pub use crate::interpreter::Interpreter;
 pub use crate::miniscript::context::{BareCtx, Legacy, ScriptContext, Segwitv0, Tap};
 pub use crate::miniscript::decode::Terminal;
